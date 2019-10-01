b8ec1dae5b3d40fad75d7e0e047c4f1d1c7abf45
André Brandão Teodoro <andrebrandaoca@gmail.com>
2019-10-01 15:24:06 -0400

    Add project files.

---
+++ Controllers/RecipesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Recipes.Controllers
+{
+    public class RecipesController:Controller
+    {
+        public ViewResult Home()
+        {
+            return View();
+        }
+        public ViewResult AddRecipe()
+        {
+            return View();
+        }
+        public ViewResult RecipeList()
+        {
+            return View();
+        }
+        public ViewResult ReviewRecipe()
+        {
+            return View();
+        }
+        public ViewResult ViewRecipe()
+        {
+            return View();
+        }
+
+    }
+}
